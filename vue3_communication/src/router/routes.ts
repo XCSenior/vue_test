4c50915b84a2099ecd71002fe63fe4b161c1f19a
XCSenior <2969271422@qq.com>
2023-06-08 21:12:56 +0800

    Add my test Component

--- vue3_communication/src/router/routes.ts
+++ vue3_communication/src/router/routes.ts
@@ -1,45 +1,47 @@
 /*
 所有路由配置的数组
 */
-
+// 页面基本路径
+// const basePath = 'views'
+// const basePath = 'views_draft'
 export default [
-  
+
 
   /* --------------------------------------- */
   {
     path: '/props_pre',
-    component: () => import('@/views/01_props/PropsTest.vue'),
+    component: () => import(`@/views_draft/01_props/PropsTest.vue`),
   },
   {
     path: '/event_pre',
-    component: () => import('@/views/02_custom-event/EventTest.vue'),
+    component: () => import(`@/views_draft/02_custom-event/EventTest.vue`),
   },
   {
     path: '/bus_pre',
-    component: () => import('@/views/03_event-bus/EventBusTest.vue'),
+    component: () => import(`@/views_draft/03_event-bus/EventBusTest.vue`),
   },
   {
     path: '/model_pre',
-    component: () => import('@/views/04_v-model/ModelTest.vue'),
+    component: () => import(`@/views_draft/04_v-model/ModelTest.vue`),
   },
   {
     path: '/attrs-listeners_pre',
-    component: () => import('@/views/05_attrs-listeners/AttrsListenersTest.vue'),
+    component: () => import(`@/views_draft/05_attrs-listeners/AttrsListenersTest.vue`),
   },
   {
     path: '/ref-parent_pre',
-    component: () => import('@/views/06_ref-children-parent/RefChildrenParentTest.vue'),
+    component: () => import(`@/views_draft/06_ref-children-parent/RefChildrenParentTest.vue`),
   },
   {
     path: '/provide-inject_pre',
-    component: () => import('@/views/07_provide-inject/ProvideInjectTest.vue'),
+    component: () => import(`@/views_draft/07_provide-inject/ProvideInjectTest.vue`),
   },
   {
     path: '/vuex_pre',
-    component: () => import('@/views/08_pinia/index.vue'),
+    component: () => import(`@/views_draft/08_pinia/index.vue`),
   },
    {
     path: '/slot_pre',
-    component: () => import('@/views/09_slot/SlotTest.vue'),
+    component: () => import(`@/views_draft/09_slot/SlotTest.vue`),
   },
 ]
