3e6b51aee4cacde74adf452b3e06b72dc2adcc15
XCSenior <77381715+XCSenior@users.noreply.github.com>
2023-06-12 01:09:45 +0800

    Feat: finish v-model test.

--- vue3_communication/src/views_draft/04_v-model/Child.vue
+++ vue3_communication/src/views_draft/04_v-model/Child.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="child">
-    <h3>钱数:{{ modelValue }}</h3>
-    <button @click="handler">父子组件数据同步</button>
+    <h3>钱数：{{ modelValue }}</h3>
+    <button @click="updateModelValue">父子组件事件同步</button>
   </div>
 </template>
 
 <script setup lang="ts">
-//接受props
-let props = defineProps(["modelValue"]);
-let $emit = defineEmits(['update:modelValue']);
-//子组件内部按钮的点击回调
-const handler = ()=>{
-   //触发自定义事件
-   $emit('update:modelValue',props.modelValue+1000);
-}
+/* 接收props */
+const props = defineProps(['modelValue']);
+const $emit = defineEmits(['update:modelValue'])
+
+const updateModelValue = () => {
+    $emit('update:modelValue', props.modelValue + 1000)
+};
 </script>
 
 <style scoped>
--- vue3_communication/src/views_draft/04_v-model/Child1.vue
+++ vue3_communication/src/views_draft/04_v-model/Child1.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="child2">
     <h1>同时绑定多个v-model</h1>
-    <button @click="handler">pageNo{{ pageNo }}</button>
-    <button @click="$emit('update:pageSize', pageSize + 4)">
-      pageSize{{ pageSize }}
-    </button>
+    <button @click="$emit('update:pageNo', pageNo + 2)">pageNo：{{ pageNo }}</button>
+    <button @click="$emit('update:pageSize', pageSize + 3)">pageSize：{{ pageSize }}</button>
   </div>
 </template>
 
 <script setup lang="ts">
-let props = defineProps(["pageNo", "pageSize"]);
-let $emit = defineEmits(["update:pageNo", "update:pageSize"]);
-//第一个按钮的事件回调
-const handler = () => {
-  $emit("update:pageNo", props.pageNo + 3);
-};
+const props = defineProps(['pageNo', 'pageSize'])
+const $emit = defineEmits(['update:pageNo', 'update:pageSize'])
+
+
 </script>
 
 <style scoped>
