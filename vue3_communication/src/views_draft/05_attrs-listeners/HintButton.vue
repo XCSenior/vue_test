abefe5c381c5c94d31d9898a02de3abc7cf0211e
XCSenior <77381715+XCSenior@users.noreply.github.com>
2023-06-14 00:04:12 +0800

    Update HintButton.vue

--- vue3_communication/src/views_draft/05_attrs-listeners/HintButton.vue
+++ vue3_communication/src/views_draft/05_attrs-listeners/HintButton.vue
@@ -7,11 +7,9 @@ import { useAttrs } from 'vue';
 
 // 引入useAttrs方法，获取组件标签身上的属性与事件
 const $attrs = useAttrs(); // 返回一个Proxy对象
-console.log('$attrs :>> ', $attrs);
 // 如果用props声明一些属性呢？
 const props = defineProps(['title']);
 // 那么useAttrs就拿不到，props已经声明的属性了
-console.log('props :>> ', props);
 
 </script>
 
