19b1601fdada9de0bb90509424ac4c910f818600
XCSenior <77381715+XCSenior@users.noreply.github.com>
2023-06-13 23:55:19 +0800

    Finish useAttrs test.

--- vue3_communication/src/views_draft/05_attrs-listeners/AttrsListenersTest.vue
+++ vue3_communication/src/views_draft/05_attrs-listeners/AttrsListenersTest.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
+    <h1>useAttrs</h1>
+    <el-button type="primary" size="small" :icon="Edit">el-button</el-button>
 
+    <!-- 自定义组件 -->
+    <HintButton type="primary" size="small" :icon="Edit" :title="'你好大傻逼'"></HintButton>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Edit } from '@element-plus/icons-vue';
+import HintButton from './HintButton.vue';
+
+
+// vue3提供一个useAttrs方法，获取组件身上的属性和方法
+
 
 </script>
 
--- vue3_communication/src/views_draft/05_attrs-listeners/HintButton.vue
+++ vue3_communication/src/views_draft/05_attrs-listeners/HintButton.vue
@@ -1,8 +1,17 @@
 <template>
-
+    <el-button v-bind="$attrs">{{ title }}</el-button>
 </template>
 
 <script setup lang="ts">
+import { useAttrs } from 'vue';
+
+// 引入useAttrs方法，获取组件标签身上的属性与事件
+const $attrs = useAttrs(); // 返回一个Proxy对象
+console.log('$attrs :>> ', $attrs);
+// 如果用props声明一些属性呢？
+const props = defineProps(['title']);
+// 那么useAttrs就拿不到，props已经声明的属性了
+console.log('props :>> ', props);
 
 </script>
 
