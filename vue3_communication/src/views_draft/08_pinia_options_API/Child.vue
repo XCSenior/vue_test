146dd20c890503256e56134809fc92b3d376eef5
XCSenior <77381715+XCSenior@users.noreply.github.com>
2023-06-17 22:58:51 +0800

    format: Pinia test demo.

--- vue3_communication/src/views_draft/08_pinia_options_API/Child.vue
+++ vue3_communication/src/views_draft/08_pinia_options_API/Child.vue
@@ -1,26 +1,17 @@
 <template>
-  <div class="child">
-    <h1>{{ infoStore.count }}---{{infoStore.total}}</h1>
-    <button @click="updateCount">点击我修改仓库数据</button>
-  </div>
+    <div class="child">
+
+    </div>
 </template>
 
 <script setup lang="ts">
-import useInfoStore from "../../store/modules/info";
-//获取小仓库对象
-let infoStore = useInfoStore();
-console.log(infoStore);
-//修改数据方法
-const updateCount = () => {
-  //仓库调用自身的方法去修改仓库的数据
-  infoStore.updateNum(66,77);
-};
+
 </script>
 
 <style scoped>
 .child {
-  width: 200px;
-  height: 200px;
-  background: yellowgreen;
+    width: 200px;
+    height: 200px;
+    background: yellowgreen;
 }
 </style>
